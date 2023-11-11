c694e3dd74c0232ea68f7ed92e21fac4d4b5dbd2
superstark01 <superstark02@gmail.com>
2023-11-11 11:51:31 +0530

    strings trimming in prog

--- src/trim-string/index.ts
+++ src/trim-string/index.ts
@@ -1,6 +1,7 @@
-import { commands, window } from "vscode";
+import { Position, Range, commands, window } from "vscode";
 import { strings } from "../constants/constants";
 import { abort } from "process";
+import { findStrings } from "./utils";
 
 export const trimStrings = commands.registerCommand(
   "mmt-extranet-git-commands.trim_strings",
@@ -12,7 +13,15 @@ export const trimStrings = commands.registerCommand(
     }
 
     const data = currentFile.document.getText();
-    console.log(data);
+    const fixedData = findStrings(data);
+
+    window.activeTextEditor?.edit((editBuilder) => {
+      const range = new Range(
+        new Position(0, 0),
+        new Position(currentFile.document.lineCount + 1, 0)
+      );
+      editBuilder.replace(range, fixedData);
+    });
     return;
   }
 );
--- src/trim-string/utils.ts
+++ src/trim-string/utils.ts
@@ -1,3 +1,49 @@
-export const removeSpaces = (string_: string) => {
+export const findStrings = (string_: string) => {
+  //find string
+  var index = 0;
+  var subStrings: Array<string> = [];
+
+  for (index = 0; index < string_.length; index++) {
+    const char = string_[index];
+    if (isQuote(char)) {
+      var endOfString = index + 1;
+      var substring = "";
+
+      while (string_[endOfString] !== char && endOfString < string_.length) {
+        substring = substring + string_[endOfString];
+        endOfString = endOfString + 1;
+      }
+
+      subStrings.push(substring);
+      index = endOfString;
+    }
+  }
+
+  subStrings.forEach((item) => {
+    string_ = string_.replace(item, removeSpaces(item.trim()));
+  });
   return string_;
+};
+
+const removeSpaces = (string_: string): string => {
+  var correctedString = "";
+  var i = 0;
+
+  while (i < string_.length) {
+    correctedString = correctedString + string_[i];
+    if (string_[i] === " ") {
+      var j = i + 1;
+      while (j < string_.length && string_[j] === " ") {
+        j++;
+      }
+      i = j;
+    } else {
+      i++;
+    }
+  }
+  return correctedString;
+};
+
+const isQuote = (char: string): boolean => {
+  return ['"', "`", "'"].includes(char);
 };
